dfff3308b447adaec56bc6a16ae76e6f1dc05cc1
Kevin Azangue <kevinromuald@gmail.com>
2024-06-27 22:00:56 +0200

    start redesign login

--- src/app/layout/app-layout/auth-layout/auth-layout.component.ts
+++ src/app/layout/app-layout/auth-layout/auth-layout.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-auth-layout',
+  selector: 'stoom-auth-layout',
   standalone: true,
   imports: [
     RouterOutlet
